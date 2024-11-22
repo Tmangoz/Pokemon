827754e0629faa555d214f917c4b990da36884c7
T-Man <167492062+Tmangoz@users.noreply.github.com>
2024-11-22 14:24:39 -0500

    Update Poke_App.py

--- Poke_App.py
+++ Poke_App.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+from io import Bytes IO #Import Bytes IO to handle binary file content
 #Page Setup
 st.set_page_config(page_title = "Pokedex",layout="wide")
 st.title("Pokemon Stat Sheet")
@@ -9,8 +10,12 @@ st.title("Pokemon Stat Sheet")
 #read excel file and create dataframe
 url= 'https://github.com/Tmangoz/Pokemon/blob/main/pokemon_data.xlsx'
 myfile = requests.get(url)
-xlsx = pd.read_excel(myfile.content)
-poke_data = pd.DataFrame(xlsx) 
 
-#Show the dataframe(Delete Later)
-print(poke_data)   
+if response.status_code = 200: #check if the request was successful
+  data = BytesIO(myfile.content)
+  poke_data = pd.read_excel(data)
+  st.dataframe(poke_data) #Display Dataframe in the Streamlit app
+else:
+  st.error("Failed to retrieve data from the provided URL.")
+
+ 
