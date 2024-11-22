7a880d9910ec03ab03b7606ef512de6e8f66512e
T-Man <167492062+Tmangoz@users.noreply.github.com>
2024-11-22 10:16:49 -0500

    Update Poke_App.py

--- Poke_App.py
+++ Poke_App.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title = "Pokedex",layout="wide")
 st.title("Pokemon Stat Sheet")
 
 #read excel file and create dataframe
-xlsx = pd.read_excel("Pokemon_App/pokemon_data.xlsx")
+xlsx = pd.read_excel("pokemon_data.xlsx")
 poke_data = pd.DataFrame(xlsx) 
 
 #Show the dataframe(Delete Later)
