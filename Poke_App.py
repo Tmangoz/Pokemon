f92095cf33b59d5b067404c6a28ab869b1866a84
T-Man <167492062+Tmangoz@users.noreply.github.com>
2024-11-22 10:20:22 -0500

    Update Poke_App.py

--- Poke_App.py
+++ Poke_App.py
@@ -1,12 +1,15 @@
 #library Import
 import streamlit as st
 import pandas as pd
+import requests
 #Page Setup
 st.set_page_config(page_title = "Pokedex",layout="wide")
 st.title("Pokemon Stat Sheet")
 
 #read excel file and create dataframe
-xlsx = pd.read_excel("pokemon_data.xlsx")
+url= 'https://github.com/Tmangoz/Pokemon/blob/main/pokemon_data.xlsx'
+myfile = requests.get(url)
+xlsx = pd.read_excel(myfile.content)
 poke_data = pd.DataFrame(xlsx) 
 
 #Show the dataframe(Delete Later)
