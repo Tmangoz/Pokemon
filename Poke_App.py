0bca2c1bcc47afdcf148c28c406e584ee4784428
T-Man <167492062+Tmangoz@users.noreply.github.com>
2024-11-22 10:11:11 -0500

    Create Poke_App.py

---
+++ Poke_App.py
@@ -0,0 +1,13 @@
+#library Import
+import streamlit as st
+import pandas as pd
+#Page Setup
+st.set_page_config(page_title = "Pokedex",layout="wide")
+st.title("Pokemon Stat Sheet")
+
+#read excel file and create dataframe
+xlsx = pd.read_excel("Pokemon_App/pokemon_data.xlsx")
+poke_data = pd.DataFrame(xlsx) 
+
+#Show the dataframe(Delete Later)
+st.write    
