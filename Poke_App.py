6ba2687cbbd149c62e65f12a0898cd364ab94196
T-Man <167492062+Tmangoz@users.noreply.github.com>
2025-03-07 11:36:56 -0500

    Update Poke_App.py

--- Poke_App.py
+++ Poke_App.py
@@ -6,7 +6,7 @@ import openpyxl
 st.set_page_config(page_title = "Pokedex",layout="wide")
 st.title("Pokemon Stat Sheet")
 #read excel file and create dataframe
-csv = pd.read_csv("pokemon_data.csv", index_col=False)
+csv = pd.read_csv("pokemon_data.csv")
 poke_data = pd.DataFrame(csv) 
 poke_data.head()
 #Drop index value column
@@ -17,6 +17,7 @@ capitalized_Answer = Answer.capitalize()
 #Remove Index Value so not displayed on print
 #User clicks search button to search the pokemon they are trying to look up
 if st.button("Search"):
+     poke_data.to_string(index=False)
     st.write(poke_data.loc[poke_data.Name==capitalized_Answer])
 else:
     print("Please check your spelling or enter a valid name of a pokemon")
