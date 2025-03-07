bcf3eda826869b3b12968deff82c943a119fad3f
T-Man <167492062+Tmangoz@users.noreply.github.com>
2025-03-07 11:33:53 -0500

    Update Poke_App.py

--- Poke_App.py
+++ Poke_App.py
@@ -10,14 +10,11 @@ csv = pd.read_csv("pokemon_data.csv", index_col=False)
 poke_data = pd.DataFrame(csv) 
 poke_data.head()
 #Drop index value column
-poke_data = poke_data.reset_index(drop=True)
 #Get User input for Pokemon they want
 Answer = str(st.text_input("Please enter the Pokemon you'd like to view"))
 #Capitalize the first letter of input as all names in CSV start with uppercase letter
 capitalized_Answer = Answer.capitalize()
 #Remove Index Value so not displayed on print
-blankIndex=[''] * len(poke_data)
-poke_data.index=blankIndex
 #User clicks search button to search the pokemon they are trying to look up
 if st.button("Search"):
     st.write(poke_data.loc[poke_data.Name==capitalized_Answer])
